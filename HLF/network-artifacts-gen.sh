375c75a00fb4cb9c3276a3eb1cf8aa32afbaf207
Collins <collinsdelan@gmail.com>
2018-03-22 15:22:02 +0300

    add cryptographic generator script

---
+++ HLF/network-artifacts-gen.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+set -ev
+
+export VERSION=1.0.2
+export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+
+MARCH=`uname -m`
+
+FABRIC="$MARCH-$VERSION"
+BIN="$ARCH-$VERSION"
+SLEEP_TIMEOUT=10
+
+ORG_DOMAIN="example.domain.com"
+
+if [ ! "$(docker images | grep hyperledger/fabric )" ]; then
+ docker pull hyperledger/fabric-peer:$FABRIC
+ docker pull hyperledger/fabric-ca:$FABRIC
+ docker pull hyperledger/fabric-ccenv:$FABRIC
+ docker pull hyperledger/fabric-orderer:$FABRIC
+ docker pull hyperledger/fabric-couchdb:$FABRIC
+fi
+
+curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${BIN}/hyperledger-fabric-${BIN}.tar.gz | tar xz
+
+sleep $SLEEP_TIMEOUT
+
+[[ $(ls -A $HOME/.composer-credentials/) ]] && rm $HOME/.composer-credentials/* || echo "All good, no old credentials found."
+
+./bin/cryptogen generate --config=./crypto-config.yaml
+
+sleep $SLEEP_TIMEOUT
+
+KEYSTORE=`ls ./crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp/keystore`
+composer identity import \
+-p hlfv1 \
+-u Admin@$ORG_DOMAIN \
+-c ./crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp/signcerts/Admin@$ORG_DOMAIN-cert.pem \
+-k ./crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp/keystore/$KEYSTORE
+
+sleep $SLEEP_TIMEOUT
+
+export FABRIC_CFG_PATH=$PWD
+
+OUTPUT_DIR=$PWD/channel-artifacts
+
+mkdir -p $OUTPUT_DIR
+
+./bin/configtxgen -profile LoyyalOrdererGenesis -outputBlock $OUTPUT_DIR/loyyal-genesis.block
+./bin/configtxgen -profile LoyyalChannel -outputCreateChannelTx $OUTPUT_DIR/loyyal-channel.tx -channelID loyyalchannel
+
+echo "... 🙌🏿"
+  
